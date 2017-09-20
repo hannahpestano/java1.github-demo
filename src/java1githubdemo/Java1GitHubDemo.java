9327e9e7653160a76c56e4144d5e91d716cc8c96
macbook <macbook@192.168.113.173>
2017-09-20 11:01:01 -0500

    first commit

---
+++ src/java1githubdemo/Java1GitHubDemo.java
@@ -0,0 +1,12 @@
+package java1githubdemo;
+
+public class Java1GitHubDemo {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello, world!");
+        
+        
+    }
+    
+}
